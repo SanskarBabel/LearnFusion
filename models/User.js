95aea92861c604c5521152c56d7aa737d5cb6cb5
SanskarBabel <sanskarbabel7742@gmail.com>
2024-06-11 18:45:47 +0530

    Add initial files

--- models/User.js
+++ models/User.js
@@ -47,7 +47,6 @@ const userSchema = new mongoose.Schema({
       }
     ],
 
-  });
+});
   
-  module.exports = mongoose.model("User", userSchema);
-  
+module.exports = mongoose.model("User", userSchema);
