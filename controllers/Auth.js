959c5a9edabe15b48fe86dfae705321636d25668
SanskarBabel <sanskarbabel7742@gmail.com>
2024-06-12 16:31:04 +0530

    Add initial files

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,4 +1,16 @@
+const User = require("../models/User");
+const OTP = require("../models/OTP");
+
 //Send OTP
+exports.sendOTP = async(req, res) => {
+
+    //Fetch Email from Reqeuest's Body
+    const {email} = req.body;
+
+    // Check if User already exist
+    const checkUserPresent = await User.findOne({email});
+
+}
 
 //Signup
 
