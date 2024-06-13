73dad5e22960b74521f1350d9578fbae39ef2091
SanskarBabel <sanskarbabel7742@gmail.com>
2024-06-13 15:47:25 +0530

    Add initial files

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -165,5 +165,118 @@ exports.signUp = async (req, res) => {
 };
 
 // Login
+const login = async (req, res) => {
+    try {
+        // Get data from request body
+        const {email, password} = req.body;
+
+        // Validation of Data
+        if(!email || !password){
+            return res.status(403). json({
+                success:false,
+                message:'Please Fill up All the Required Fields',
+            });
+        }
+        
+        // User check
+        const user = await User.findOne({email}).populate("additionalDetails");
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:"User is not registrered, please signup first",
+            });
+        }
+        
+        if(await bcrypt.compare(password, user.password)){
+            const payload = {
+                email: user.email,
+                id: user._id,
+                accountType:user.accountType,
+            }
+
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {
+                expiresIn:"20h",
+            });
+
+            user.token = token;
+            user.password= undefined;
+
+            const options = {
+                expires: new Date(Date.now() + 3*24*60*60*1000),
+                httpOnly:true,
+            }
+
+            res.cookie("token", token, options).status(200).json({
+                success:true,
+                token,
+                user,
+                message:'Logged in successfully',
+            })
+        }
+        else {
+            return res.status(401).json({
+                success:false,
+                message:'Password is incorrect',
+            });
+        };
+    }
+    catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:'Login Failure, please try again',
+        });
+    }
+};
 
 // Change Password
+const changePassword = async (req, res) => {
+	try {
+		const userDetails = await User.findById(req.user.id);
+		const { oldPassword, newPassword, confirmNewPassword } = req.body;
+
+		const isPasswordMatch = await bcrypt.compare(oldPassword, userDetails.password );
+			 
+		if(!isPasswordMatch) {
+			return res.status(401).json({ success: false, message: "The password is incorrect" });	 
+		}
+
+		if(newPassword !== confirmNewPassword) {
+            return res.status(401).json({ 
+            success: false,
+            message: "The password and confirm password does not match" });	 
+		}
+			 
+		const encryptedPassword = await bcrypt.hash(newPassword, 10);
+		const updatedUserDetails = await User.findByIdAndUpdate(req.user.id , { password: encryptedPassword } , { new: true });
+		 
+		try {     
+			const emailResponse = await mailSender(updatedUserDetails.email,
+            passwordUpdated(updatedUserDetails.email,
+            `Password updated successfully for ${updatedUserDetails.firstName} ${updatedUserDetails.lastName}`));
+			console.log("Email sent successfully:", emailResponse.response);
+		}
+        catch(error) {
+			return res.status(500).json({
+				success: false,
+				message: "Error occurred while sending email",
+				error: error.message,
+			});
+		}
+
+		return res.status(200).json({ 
+            success: true, 
+            message: "Password updated successfully" 
+        });
+	} 
+    catch(error) {
+		console.error("Error occurred while updating password:", error);
+		return res.status(500).json({
+			success: false,
+			message: "Error occurred while updating password",
+			error: error.message,
+		});
+	}
+};
+
+module.exports =  {signUp , login , sendOTP , changePassword};
