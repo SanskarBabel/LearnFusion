041e1b926635ada066a4e79ffe3ad48a347e03f1
SanskarBabel <sanskarbabel7742@gmail.com>
2024-06-29 03:35:29 +0530

    Add initial files

---
+++ routes/User.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const { login, signUp, sendOTP, changePassword,} = require("../controllers/Auth");
+const { resetPasswordToken,  resetPassword,} = require("../controllers/ResetPassword");
+const { auth } = require("../middlewares/auth");
+
+router.post("/signup", signUp);
+router.post("/sendotp", sendOTP);
+router.post("/changepassword", auth, changePassword);
+
+router.post("/reset-password-token", resetPasswordToken);
+router.post("/reset-password", resetPassword);
+
+module.exports = router;
