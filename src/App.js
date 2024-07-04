de38953bd6dad1a422b47b3cda005428a4c8f8f3
SanskarBabel <sanskarbabel7742@gmail.com>
2024-07-05 00:22:52 +0530

    Add initial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useEffect } from "react";
 import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import {Route, Routes, useNavigate} from "react-router-dom";
+
+// Components
+import Navbar from "./components/common/Navbar"
+import OpenRoute from "./components/core/Auth/OpenRoute"
+import PrivateRoute from "./components/core/Auth/PrivateRoute"
+import AddCourse from "./components/core/Dashboard/AddCourse"
+import Cart from "./components/core/Dashboard/Cart"
+import EditCourse from "./components/core/Dashboard/EditCourse"
+import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses"
+import Instructor from "./components/core/Dashboard/Instructor"
+import MyCourses from "./components/core/Dashboard/MyCourses"
+import MyProfile from "./components/core/Dashboard/MyProfile"
+import Settings from "./components/core/Dashboard/Settings"
+import VideoDetails from "./components/core/ViewCourse/VideoDetails"
+import About from "./pages/About"
+import Catalog from "./pages/Catalog"
+import Contact from "./pages/Contact"
+import CourseDetails from "./pages/CourseDetails"
+import Dashboard from "./pages/Dashboard"
+import Error from "./pages/Error"
+import ForgotPassword from "./pages/ForgotPassword"
+// Pages
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import UpdatePassword from "./pages/UpdatePassword"
+import VerifyEmail from "./pages/VerifyEmail"
+import ViewCourse from "./pages/ViewCourse"
+import { getUserDetails } from "./services/operations/profileAPI"
+import { ACCOUNT_TYPE } from "./utils/constants"
 
 function App() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.profile);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+      </Routes>
     </div>
   );
 }
